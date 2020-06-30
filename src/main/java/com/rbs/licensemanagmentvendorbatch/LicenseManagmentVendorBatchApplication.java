b07cf57e53fef58d9d21ed82d5fb682cef799c14
Ahmed AlSalih <Ahmed.Al-Salih@retailbusinessservices.com>
2020-06-30 16:51:00 -0400

    springboot Batch JPA

---
+++ src/main/java/com/rbs/licensemanagmentvendorbatch/LicenseManagmentVendorBatchApplication.java
@@ -0,0 +1,13 @@
+package com.rbs.licensemanagmentvendorbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseManagmentVendorBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseManagmentVendorBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbs/licensemanagmentvendorbatch/LicenseManagmentVendorBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbs.licensemanagmentvendorbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseManagmentVendorBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
